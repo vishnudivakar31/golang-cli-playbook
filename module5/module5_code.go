7fc272c04897489de52edb58f3ae8def7b018064
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-08-05 09:20:17 -0400

    Completed module

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,12 +1,17 @@
 package module5
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 
-/*func GetExampleDotCom() {
+func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
 		fmt.Println("something went wrong")
 	}
 
 	defer resp.Body.Close()
-}*/
+}
